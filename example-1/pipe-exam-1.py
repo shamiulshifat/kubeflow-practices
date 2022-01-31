cfc85f14e3a49d4f10b243f91e4774aeb9898b10
shamiul islam shifat <shifat.ruet@gmail.com>
2022-01-31 14:02:53 +0600

    Initial commit

---
+++ example-1/pipe-exam-1.py
@@ -0,0 +1,31 @@
+import kfp
+from kfp import dsl
+
+@dsl.pipeline(name="my first pipeline")
+def pipeline():
+    op0=dsl.ContainerOp(
+        name="number generator",
+        image="python:alpine3.6",
+        command=["sh", "-c"],
+        arguments=[
+            'python -c "import random; import json; import sys; json.dump([i for i in range(20, 26)], open(\'/tmp/out.json\', \'w\'))"'],
+        file_outputs={"out": "/tmp/out.json"},   
+        
+    )
+    with dsl.ParallelFor(op0.output) as item:
+        op1=dsl.ContainerOp(
+            name="item printer",
+            image="library/bash:4.4.23",
+            command=["sh", "-c"],
+            arguments=["echo do output op1 item: %s" %item],
+        )
+    op_out=dsl.ContainerOp(
+        name="total",
+        image="python:alpine3.6",
+        command=["sh", "-c"],
+        arguments=['echo output gen-numbers: %s && python -c "print(sum(%s))"' % (op0.output, op0.output)],
+
+    )
+
+if __name__ == '__main__':
+    kfp.compiler.Compiler().compile(pipeline, __file__ + '.yaml')
